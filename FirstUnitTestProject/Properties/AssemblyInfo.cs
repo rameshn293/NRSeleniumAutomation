574cd38b0362752e9c6217b0754f5029e5c74299
Ramesh Nagendrappa <rameshn293@gmail.com>
2020-10-27 16:27:03 +0530

    Project Added

---
+++ FirstUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FirstUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("FirstUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ddadd0eb-f8fe-4ad6-be64-6249baf4ac2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FirstUnitTestProject/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       static IWebDriver driver = new ChromeDriver();
+        
+        [ClassInitialize]
+        public static void Initialize(TestContext test)
+        {
+            //IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demosite.executeautomation.com/index.html?UserName=&Password=&Login=Login");
+            Console.WriteLine("URL Opened");
+        }
+
+        [TestMethod]
+        public void ExeuteTest()
+        {
+            //Title
+            SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+
+            // Initial
+
+            SeleniumSetMethods.EnterText(driver, "Initial", "N", "Name");
+
+            //First Name
+            SeleniumSetMethods.EnterText(driver, "FirstName", "Ramesh", "Name");
+
+            //MiddleName
+            SeleniumSetMethods.EnterText(driver, "MiddleName", "Nagendrappa", "Name");
+
+            //Click
+
+            SeleniumSetMethods.Click(driver,"Save","Name");
+
+        }
+
+        [TestMethod]
+        public void Login()
+        {
+            
+           IWebElement username  = driver.FindElement(By.Id("username"));
+            username.SendKeys("neetprodins1");
+            Console.WriteLine("Username Entered");
+
+            IWebElement password = driver.FindElement(By.Id("password"));
+            password.SendKeys("Password1");
+            Console.WriteLine("Password ENtered");
+
+           IWebElement signin = driver.FindElement(By.Id("mainButton"));
+           signin.Click();
+            Console.WriteLine("Signin Button Clicked");
+        }
+        [TestMethod]
+        public void NextTest1()
+        {
+            Console.WriteLine("NextTest 1");
+
+        }
+
+        [TestMethod]
+        public void NextTest2()
+        {
+            Console.WriteLine("NextTest 2");
+
+        }
+
+        [TestMethod]
+        public void NextTest3()
+        {
+            Console.WriteLine("NextTest 3");
+
+        }
+
+        [TestCleanup]
+
+        public void Cleanup()
+        {
+            //driver.Close();
+            //Console.WriteLine("Browser Closed");
+        }
+    }
+}
